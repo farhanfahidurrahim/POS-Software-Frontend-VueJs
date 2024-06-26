ce253f38a56a99be5af6c6e7cfc129f28579c478
farhanfahidurrahim <farhan.fahidurrahim@gmail.com>
2024-06-26 12:46:35 +0600

    first commit

---
+++ src/components/Brand.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Brands</h1>
+    <!-- <div v-if="brands.length"></div> -->
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const brands = ref 
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2>Welcome to Vue...</h2>
+</template>
+
+<script></script>
+
+<style scoped>
+/* Styles specific to Home.vue */
+</style>
