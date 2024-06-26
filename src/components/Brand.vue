340fb77651f19a3e11282d8bf8e7fdb70db6f461
farhanfahidurrahim <farhan.fahidurrahim@gmail.com>
2024-06-26 14:44:41 +0600

    brand api fetch

--- src/components/Brand.vue
+++ src/components/Brand.vue
@@ -6,8 +6,22 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
+import { ref, onMounted } from "vue";
+import axios from "axios";
 
-const brands = ref 
+const brands = ref([]);
+
+const fetchBrands = async () => {
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_BASE_API_URL}/api/v1/brands`
+    );
+    console.log("response", response);
+    brands.value = response.data.data;
+  } catch (error) {
+    console.log("Error fetching:", error);
+  }
+};
+
+onMounted(fetchBrands);
 </script>
