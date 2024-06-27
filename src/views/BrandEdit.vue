bb95508cbe9bfda9274f34c3b1be687701083848
Farhan Fahidur Rahim <farhan.fahidurrahim@gmail.com>
2024-06-27 21:37:36 +0600

    brand edit: category fxd

--- src/views/BrandEdit.vue
+++ src/views/BrandEdit.vue
@@ -13,6 +13,7 @@
   </div>
   <div>
     <form @submit.prevent="updateBrand">
+
       <div class="mb-3">
         <label for="name" class="form-label">Name:</label>
         <input
@@ -21,7 +22,21 @@
           id="name"
           v-model="brand.name"
         />
+        <div v-if="errors.name" class="text-danger">
+          {{ errors.name[0] }}
+        </div>
       </div>
+
+      <div class="mb-3">
+        <label for="category" class="form-label">Category: <span style="color: red;">*</span></label>
+        <select id="category" v-model="brand.category_id" class="form-control">
+          <option v-for="category in categories" :key="category.id" :value="category.id">{{ category.name }}</option>
+        </select>
+        <div v-if="errors.category_id" class="text-danger">
+          {{ errors.category_id[0] }}
+        </div>
+      </div>
+
       <div class="mb-3">
         <label for="description" class="form-label">Description:</label>
         <textarea
@@ -42,19 +57,40 @@ import { useRoute, useRouter } from "vue-router";
 
 const route = useRoute();
 const router = useRouter();
+const errors = ref({});
 
 const brand = ref({
   name: "",
   description: "",
 });
 
+const categories = ref([]);
+
+const fetchCategories = async () => {
+  try {
+    const response = await axios.get("/api/v1/categories");
+    categories.value = response.data.data;
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+  }
+};
+
 const fetchBrand = async () => {
   try {
     const response = await axios.get(`/api/v1/brands/${route.params.id}`);
-    brand.value = response.data.data;
+    const data = response.data.data;
+    brand.value = {
+      name: data.name,
+      category_id: data.category.id,
+      description: data.description,
+    };
     console.log("Response=>", response);
   } catch (error) {
-    console.error("Error fetching brand:", error);
+    if (error.response && error.response.data.errors) {
+      errors.value = error.response.data.errors;
+    } else {
+      console.error("Error:", error);
+    }
   }
 };
 
@@ -68,6 +104,7 @@ const updateBrand = async () => {
 };
 
 onMounted(() => {
+  fetchCategories();
   fetchBrand();
 });
 </script>
