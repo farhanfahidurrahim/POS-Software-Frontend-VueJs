321abe3bad9a5999fdcfee3c6914553d85194585
farhanfahidurrahim <farhan.fahidurrahim@gmail.com>
2024-07-01 18:48:43 +0600

    sale report dynamci

--- src/views/Report/SalesReport.vue
+++ src/views/Report/SalesReport.vue
@@ -11,25 +11,25 @@
       <!-- Row 1 -->
       <div class="col-md-3">
         <div class="p-3 border bg-light">
-          <p>100</p>
+          <p>{{ fetchSaleReport.total_sale }}</p>
           <h6>Total Orders</h6>
         </div>
       </div>
       <div class="col-md-3">
         <div class="p-3 border bg-light">
-          <p>100</p>
+          <p>{{ fetchSaleReport.total_saleQuantity }}</p>
           <h6>Total Orders Quantity</h6>
         </div>
       </div>
       <div class="col-md-3">
         <div class="p-3 border bg-light">
-          <p>100</p>
+          <p>{{ fetchSaleReport.total_saleAmount }}</p>
           <h6>Total Orders Amount</h6>
         </div>
       </div>
       <div class="col-md-3">
         <div class="p-3 border bg-light">
-          <p>100</p>
+          <p>{{ fetchSaleReport.total_paidAmount }}</p>
           <h6>Total Paid Amount</h6>
         </div>
       </div>
@@ -38,16 +38,28 @@
     <div class="row mt-3">
       <!-- Row 2 -->
       <div class="col-md-3">
-        <div class="p-3 border bg-light">Column 1</div>
+        <div class="p-3 border bg-light">
+          <p>{{ fetchSaleReport.total_paidAmount }}</p>
+          <h6>Total Due Amount</h6>
+        </div>
       </div>
       <div class="col-md-3">
-        <div class="p-3 border bg-light">Column 2</div>
+        <div class="p-3 border bg-light">
+          <p>{{ fetchSaleReport.total_shippingCharge }}</p>
+          <h6>Total Shipping Charge</h6>
+        </div>
       </div>
       <div class="col-md-3">
-        <div class="p-3 border bg-light">Column 3</div>
+        <div class="p-3 border bg-light">
+          <p>{{ fetchSaleReport.total_cancelSale }}</p>
+          Total Cancel Order
+        </div>
       </div>
       <div class="col-md-3">
-        <div class="p-3 border bg-light">Column 4</div>
+        <div class="p-3 border bg-light">
+          <p>{{ fetchSaleReport.total_cancelSaleAmount }}</p>
+          Total Cancel Amount
+        </div>
       </div>
     </div>
   </div>
@@ -60,7 +72,16 @@ import { useToast } from "vue-toastification";
 
 const toast = useToast();
 const loading = ref(false);
-const fetchSaleReport = ref("");
+const fetchSaleReport = ref({
+  total_sale: 0,
+  total_saleQuantity: 0,
+  total_saleAmount: 0,
+  total_paidAmount: 0,
+  total_dueAmount: 0,
+  total_shippingCharge: 0,
+  total_cancelSale: 0,
+  total_cancelSaleAmount: 0,
+});
 
 const fetchData = async (url = "api/v1/report/sale") => {
   try {
